0d250b2f039e40140e0d990e2a8705e17a2b2633
Plamen Simeonov <haibusa1991@gmail.com>
2023-12-06 23:59:17 +0200

    initial commit

---
+++ src/main/java/com/personal/mrt/MrtApplication.java
@@ -0,0 +1,13 @@
+package com.personal.mrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/mrt/MrtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.mrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
